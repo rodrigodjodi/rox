5e0e3d38332fe86cdff7dbc10f89e4a0a35f2554
rodrigodjodi <rodrigo.djodi@gmail.com>
2018-11-29 17:35:30 -0200

    Header e bottom nav.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app>
     <v-toolbar app>
+      <img src="rox-logo.png" alt="logo rox" id="roxlogo">
       <v-toolbar-title class="headline text-uppercase">
-        <span>ROX</span>
-        <span class="font-weight-light">CABRAL</span>
+        <span class="font-weight-light">{{title}}</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
@@ -11,6 +11,13 @@
     <v-content>
       <router-view></router-view>
     </v-content>
+
+    <v-bottom-nav :active.sync="bottomNav" :color="color" :value="true" app>
+      <v-btn dark v-for="(item, index) in navitems" :key="index">
+        <span>{{item.texto}}</span>
+        <v-icon>{{item.icone}}</v-icon>
+      </v-btn>
+    </v-bottom-nav>
   </v-app>
 </template>
 
@@ -20,8 +27,21 @@ export default {
   components: {},
   data() {
     return {
-      //
+      navitems: [
+        { texto: "Maquete", icone: "360" },
+        { texto: "Tour", icone: "pin_drop" },
+        { texto: "Imagens", icone: "collections" }
+      ],
+      bottomNav: 0,
+      color: "#5e354c",
+      showNav: true,
+      title: "MAquete Virtual"
     };
   }
 };
 </script>
+<style>
+#roxlogo {
+  height: 48px;
+}
+</style>
